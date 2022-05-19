7603897c3884f3c67cfa2dbb96e3cc6c91bee195
Evan Anderson <40438869+TheeeEVan@users.noreply.github.com>
2022-05-19 21:43:50 +0000

    lots of ui changes

--- js/darkmode.js
+++ js/darkmode.js
@@ -2,10 +2,28 @@
 
 darkmode.js
 ----------------
-darkmode.js is a simple script that toggles the darkmode class on the body element.
+darkmode.js is a simple script that toggles the darkmode class on the body element. also checks for system dark mode, and stores setting in cookies
 
 */
 
-function toggleDarkMode() {
-    document.get
+/* check for dark mode in cookies */
+if (document.cookie.split("=")[0] == "dark-mode") {
+	if (document.cookie.split("=")[1] == "1")
+	{
+		document.body.classList.toggle("dark-mode")
+		document.getElementById("dark-mode-toggle").checked = "checked"
+	}
 }
+/* checks if dark mode is on in os settings */
+/* https://stackoverflow.com/a/57795495 */
+else if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    // dark mode on
+	document.body.classList.toggle("dark-mode")
+	document.getElementById("dark-mode-toggle").checked = "checked"
+}
+
+/* listen for user click on switch */
+document.getElementById("dark-mode-toggle").addEventListener("click", () => {
+	document.body.classList.toggle("dark-mode")
+	document.cookie = "dark-mode=" + (document.body.classList.contains("dark-mode") ? "1" : "0")
+})
